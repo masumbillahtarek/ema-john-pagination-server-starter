68f0a711716869394018c73d2a75c2095812afb8
Masum Billah Tarek <mdmasumbillah245524@gmail.com>
2025-08-20 06:41:13 +0300

    1st

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.use(express.json());
 
 
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0h4513.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,19 +30,29 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+
+      const page=parseInt(req.query.page)
+      const size=parseInt(req.query.size)
+      console.log('Pagination Query :',req.query,page,size)
+        const result = await productCollection.find().skip(page*size).limit(size).toArray();
         res.send(result);
     })
-
+    app.get('/productsCount',async(req,res)=>{
+      const count=await productCollection.estimatedDocumentCount()
+      res.send({ count })
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
+
+
+
 
 
 app.get('/', (req, res) =>{
